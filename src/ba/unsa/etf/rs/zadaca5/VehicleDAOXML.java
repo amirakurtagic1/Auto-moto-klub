7ea1a59899d8234b861fcda89d5341e1f489e560
Amira Kurtagić <akurtagic1@etf.unsa.ba>
2020-06-15 14:32:40 +0200

    Popunjene metode koje dodaju novo vozilo, dodaju proizvodjaca ako ne postoji, kao i brisanje vozila

--- src/ba/unsa/etf/rs/zadaca5/VehicleDAOXML.java
+++ src/ba/unsa/etf/rs/zadaca5/VehicleDAOXML.java
@@ -1,15 +1,14 @@
 package ba.unsa.etf.rs.zadaca5;
 
-import javafx.application.Platform;
 import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-
 import java.beans.XMLDecoder;
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.beans.XMLEncoder;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 
 public class VehicleDAOXML implements VehicleDAO {
@@ -23,9 +22,9 @@ public class VehicleDAOXML implements VehicleDAO {
                             new FileInputStream("owners.xml")));
                 Object object = d.readObject();
                 ArrayList<Owner> arrayOfOwners = (ArrayList<Owner>) object;
+            System.out.println(arrayOfOwners.get(0).getDateOfBirth());
                 for(int i = 0; i < arrayOfOwners.size(); i++){
                     owners.add(arrayOfOwners.get(i));
-                    System.out.println(arrayOfOwners.get(i).getName() + " " + arrayOfOwners.get(i).getLivingPlace() + " " + arrayOfOwners.get(i).getDateOfBirth() );
                 }
 
         } catch (FileNotFoundException e) {
@@ -86,6 +85,7 @@ public class VehicleDAOXML implements VehicleDAO {
                             new FileInputStream("manufacturers.xml")));
             Object object =d.readObject();
             ArrayList<Manufacturer> arrayOfManufacturer = (ArrayList<Manufacturer>) object;
+            Collections.sort(arrayOfManufacturer, Comparator.comparing(Manufacturer::getName));
             for(int i = 0; i < arrayOfManufacturer.size(); i++){
                 manufacturers.add(arrayOfManufacturer.get(i));
             }
@@ -98,6 +98,7 @@ public class VehicleDAOXML implements VehicleDAO {
 
     @Override
     public void addOwner(Owner owner) {
+
 
     }
 
@@ -113,8 +114,43 @@ public class VehicleDAOXML implements VehicleDAO {
 
     @Override
     public void addVehicle(Vehicle vehicle) {
-
+        boolean yes = false;
+        yes = doesOwnerExist(vehicle.getOwner());
+        addManufacturerIfDoesntExist(vehicle.getManufacturer());
+        if(yes == false) throw new IllegalArgumentException("Vlasnik ne postoji");
+        ObservableList<Vehicle> vehicles = getVehicles();
+        vehicles.add(vehicle);
+        ArrayList<Vehicle> arrayOfVehicles = new ArrayList<>();
+        for(int i = 0; i < vehicles.size();i++){
+            arrayOfVehicles.add(vehicles.get(i));
+        }
+        Object object = arrayOfVehicles;
+        XMLEncoder e = null;
+        try {
+            e = new XMLEncoder(new BufferedOutputStream(new FileOutputStream("vehicles.xml")));
+            e.writeObject(object);
+            e.close();
+        } catch (FileNotFoundException fileNotFoundException) {
+            fileNotFoundException.printStackTrace();
+        }
     }
+
+    private boolean doesOwnerExist(Owner owner){
+        ObservableList<Owner> owners = getOwners();
+        for(Owner x : owners){
+            if(x.getJmbg().equals(owner.getJmbg())) return true;
+        }
+        return false;
+    }
+
+    private void addManufacturerIfDoesntExist(Manufacturer manufacturer){
+        ObservableList<Manufacturer> manufacturers = getManufacturers();
+        for(Manufacturer x: manufacturers) {
+            if(x.getName().equals(manufacturer.getName())) return;
+        }
+        addManufacturer(manufacturer);
+    }
+
 
     @Override
     public void changeVehicle(Vehicle vehicle) {
@@ -123,11 +159,46 @@ public class VehicleDAOXML implements VehicleDAO {
 
     @Override
     public void deleteVehicle(Vehicle vehicle) {
-
+        ObservableList<Vehicle> vehicles = getVehicles();
+        vehicles.remove(vehicle.getId()-1);
+        ArrayList<Vehicle> arrayOfVehicles = new ArrayList<>();
+        for(int i = 0; i < vehicles.size();i++){
+            arrayOfVehicles.add(vehicles.get(i));
+        }
+        Object object = arrayOfVehicles;
+        XMLEncoder e = null;
+        try {
+            e = new XMLEncoder(new BufferedOutputStream(new FileOutputStream("vehicles.xml")));
+            e.writeObject(object);
+            e.close();
+        } catch (FileNotFoundException fileNotFoundException) {
+            fileNotFoundException.printStackTrace();
+        }
     }
 
     @Override
     public void addManufacturer(Manufacturer manufacturer) {
+        int maxId = 0;
+            ObservableList<Manufacturer> manufacturers = getManufacturers();
+            Collections.sort(manufacturers, Comparator.comparing(Manufacturer::getId));
+        for(int i = 0; i < manufacturers.size(); i++){
+                if(i == manufacturers.size() - 1) maxId = manufacturers.get(i).getId();
+            }
+            manufacturer.setId(maxId + 1);
+            manufacturers.add(manufacturer);
+            ArrayList<Manufacturer> arrayOfManufacturer = new ArrayList<>();
+            for(int i = 0; i < manufacturers.size();i++) {
+                arrayOfManufacturer.add(manufacturers.get(i));
+            }
+        Object object = arrayOfManufacturer;
+        XMLEncoder e = null;
+        try {
+            e = new XMLEncoder(new BufferedOutputStream(new FileOutputStream("manufacturers.xml")));
+            e.writeObject(object);
+            e.close();
+        } catch (FileNotFoundException fileNotFoundException) {
+            fileNotFoundException.printStackTrace();
+        }
 
     }
 
